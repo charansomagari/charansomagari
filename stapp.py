45b648d7c5087c5e637c47ed1ca2ddf45bce349b
charansomagari <127735946+charansomagari@users.noreply.github.com>
2025-08-12 11:07:04 +0530

    stapp.py

--- stapp.py
+++ stapp.py
@@ -5,3 +5,10 @@ name = st.text_input("Enter your name:")
 if name:
     st.success(f"Welcome, {name}!")
 st.write('this is awesome')
+
+
+st.title("👋 Hi, I'm Charan Kumar Reddy")
+st.subheader("Senior Data Analyst | Data Engineer | Dashboard Creator")
+st.write("6+ years of experience in data analytics, visualization, and cloud technologies.")
+
+st.text_input()
