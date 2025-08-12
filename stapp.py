0b27f47e06d1de3463d46aed3cf017bb182e100a
charansomagari <127735946+charansomagari@users.noreply.github.com>
2025-08-12 11:38:53 +0530

    stapp.py

--- stapp.py
+++ stapp.py
@@ -12,3 +12,30 @@ st.subheader("Senior Data Analyst | Data Engineer | Dashboard Creator")
 st.write("6+ years of experience in data analytics, visualization, and cloud technologies.")
 
 st.text_input('yes')
+
+
+name = st.text_input("Enter your name",
+                     placeholder="Type here...")
+
+age = st.text_input("Enter your age",
+                     placeholder="Type here...")
+
+st.header(f"welcome {name}")
+
+# for i in range(9):
+#     st.write(i+1)
+
+state = {'':'nothing selected','ANDHRA PRADESH':'AMARAVATHI',
+        'TELANGANA':'HYDERABAD'}
+
+
+sel = st.selectbox('OPTIONS',['','ANDHRA PRADESH','TELANGANA'])
+
+if sel:
+    st.write(state[sel])
+    
+
+option = st.selectbox(
+    "Choose your favorite fruit:",
+    ["Apple", "Banana", "Mango", "Orange"]
+)
