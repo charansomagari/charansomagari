d74f15fc8d967393ec9d6424a6474b037a8aeaf0
charansomagari <127735946+charansomagari@users.noreply.github.com>
2025-08-14 00:35:55 +0530

    welcome.py

--- welcome.py
+++ welcome.py
@@ -1,27 +1,34 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns(3)
-col2.header("YOUR TEXT MAKER")
+
+st.title("MAKING YOUR OWN TEXT FILE")
 
 # Bigger input box
 text_data = st.text_area("", placeholder="Enter some text here", height=200)
 
 # List inputs
-t1 = st.text_area("Item 1", 'list 1')
-t2 = st.text_area("Item 2", 'list 2')
-t3 = st.text_area("Item 3", 'list 3')
-t4 = st.text_area("Item 4", 'list 4')
-t5 = st.text_area("Item 5", 'list 5')
+t1 = st.text_input("", placeholder='list 1')
+t2 = st.text_input("", placeholder='list 2')
+t3 = st.text_input("", placeholder='list 3')
+t4 = st.text_input("", placeholder='list 4')
+t5 = st.text_input("", placeholder='list 5')
 
-# Format list neatly
-t = f"""Your list was:
-1. {t1}
-2. {t2}
-3. {t3}
-4. {t4}
-5. {t5}
+# # Format list neatly
+# t = f"""Your list was:
+# 1. {t1}
+# 2. {t2}
+# 3. {t3}
+# 4. {t4}
+# 5. {t5}
+# """
+
+t = f""" text_data,
+ {t1}
+ {t2}
+ {t3}
+ {t4}
+ {t5}
 """
 
 # Download buttons
-st.download_button("Download TXT", data=text_data, file_name="myfile.txt")
-st.download_button("Download List TXT", data=t, file_name="list.txt")
+st.download_button("Download TXT", data=t, file_name="myfile.txt")
