7ad95438488594effd9dc654ff48cec0d5e5f84f
charansomagari <127735946+charansomagari@users.noreply.github.com>
2025-08-13 23:59:06 +0530

    welcome.py

--- welcome.py
+++ welcome.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-col1,col2,col3 = st.columns(2)
+col1,col2 = st.columns(2)
 col2.title("YOUT TEXT MAKER")
 text_data = st.text_input("",placeholder = "enter some text here")
 st.download_button("Download TXT", data=text_data, file_name="myfile.txt")
