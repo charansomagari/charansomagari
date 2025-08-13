1ea1ef2f4293fc917941e17cd0768e13ac14daf3
charansomagari <127735946+charansomagari@users.noreply.github.com>
2025-08-13 11:30:03 +0530

    welcome.py

--- welcome.py
+++ welcome.py
@@ -5,5 +5,8 @@ today = date.today()
 c1,c2,c3 = st.columns([1,2,1])
 
 c1.title("Hello")
-c2.subheader(today)
+c2.title(today)
+
+if st.button('welcome'):
+  st.write('welcome to this page, this page is under building, please visit again')
 
